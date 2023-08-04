9c1688ae4c3d9d2a4be9fdc0329d0daa78eb7b7a
Mohamed93390 <141321984+Mohamed93390@users.noreply.github.com>
2023-08-04 14:36:51 +0200

    Jobs

--- app.py
+++ app.py
@@ -2,9 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Clichy-Sous-Bois (93)',
+        'salary': '40 000.00€',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Aulnay-Sous-Bois (93)',
+        'salary': '48 000.00€',
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Coubron (93)',
+        'salary': '32 000.00€',
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Montfermeil (93)',
+        'salary': '35 000.00€',
+    },
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', 
+                           jobs=JOBS,
+                           company_name='EkimCorp')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
